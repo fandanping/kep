fc77db2b692ebda002f2c83464f2b923972691f2
AfterWin <CJ_Zheng1023@hotmail.com>
2016-11-07 17:27:51 +0800

    基本架构搭建

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 app.use(require('body-parser')());
+//引入session中间件
+var session = require('express-session');
 app.set('port', process.env.PORT || 3000);
 
 
@@ -17,7 +19,37 @@ var handlebars =require('express3-handlebars').create({
 });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-//引入session中间件
-app.use(require('express-session')());
+app.use(session({
+    secret: '123',
+    cookie: {maxAge: 3600000},
+    rolling: true,
+    resave: true,
+    saveUninitialized: true
+}));
 //配置静态资源
 app.use(express.static(__dirname+'/static'));
+
+app.get('/', function(req, res){
+    res.render("index");
+})
+
+
+//定制404页面
+app.use(function(req, res){
+    //res.type('text/plain');
+    res.status(404);
+    //res.send('error 404');
+    res.render('404');
+})
+//定制500页面
+app.use(function(req, res){
+    //res.type('text/plain');
+    res.status(500);
+    //res.send('error 500');
+    res.render('500');
+})
+
+//配置服务器监听
+app.listen(app.get('port'), function(){
+    console.log("server start");
+})
