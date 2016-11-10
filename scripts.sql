6cc0f7118546bae40f02d7087ae6db27193455b8
AfterWin <CJ_Zheng1023@hotmail.com>
2016-11-10 11:05:06 +0800

    数据库脚本

---
+++ scripts.sql
@@ -0,0 +1,175 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : kep
+Source Server Version : 50540
+Source Host           : 10.113.16.188:3306
+Source Database       : kepdb
+
+Target Server Type    : MYSQL
+Target Server Version : 50540
+File Encoding         : 65001
+
+Date: 2016-11-10 11:02:29
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `comment`
+-- ----------------------------
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE `comment` (
+  `Id` varchar(32) NOT NULL DEFAULT '',
+  `content` varchar(255) DEFAULT NULL COMMENT '回帖内容',
+  `topic_id` varchar(32) NOT NULL,
+  `user_id` varchar(32) NOT NULL,
+  `create-time` varchar(32) DEFAULT NULL,
+  `stat_id` int(32) NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `fk_comment_1` (`topic_id`),
+  KEY `fk_comment_2` (`user_id`),
+  KEY `fk_comment_3` (`stat_id`),
+  CONSTRAINT `fk_comment_3` FOREIGN KEY (`stat_id`) REFERENCES `statistic` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_comment_1` FOREIGN KEY (`topic_id`) REFERENCES `topic` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_comment_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of comment
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `statistic`
+-- ----------------------------
+DROP TABLE IF EXISTS `statistic`;
+CREATE TABLE `statistic` (
+  `totalcommunity` int(32) DEFAULT NULL,
+  `totaltposts` int(32) DEFAULT NULL,
+  `totalreplies` int(32) DEFAULT NULL,
+  `id` int(32) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `totalcommunity` (`totalcommunity`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of statistic
+-- ----------------------------
+INSERT INTO `statistic` VALUES ('1', '0', '0', '1');
+
+-- ----------------------------
+-- Table structure for `topic`
+-- ----------------------------
+DROP TABLE IF EXISTS `topic`;
+CREATE TABLE `topic` (
+  `Id` varchar(32) NOT NULL DEFAULT '' COMMENT '帖子Id',
+  `title` varchar(255) DEFAULT NULL COMMENT '帖子标题',
+  `create_time` varchar(255) DEFAULT NULL COMMENT '帖子创建时间',
+  `content` text COMMENT '内容',
+  `page-view` int(11) DEFAULT NULL COMMENT '浏览量',
+  `reply_num` int(11) DEFAULT NULL COMMENT '回复量',
+  `praise` int(11) DEFAULT NULL COMMENT '点赞数',
+  `user_id` varchar(32) DEFAULT NULL,
+  `statistic_id` int(32) DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `fk_topic_1` (`user_id`),
+  KEY `fk_topic_3` (`statistic_id`),
+  CONSTRAINT `fk_topic_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_topic_3` FOREIGN KEY (`statistic_id`) REFERENCES `statistic` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of topic
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `Id` varchar(32) NOT NULL,
+  `user_name` varchar(255) DEFAULT NULL COMMENT '用户名',
+  `password` varchar(255) DEFAULT NULL COMMENT '用户密码',
+  `mail` varchar(255) DEFAULT NULL COMMENT '邮箱',
+  `photo` text COMMENT '头像',
+  `signature` varchar(255) DEFAULT NULL COMMENT '个性签名',
+  `statistic_id` int(32) NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `fk_user_1` (`statistic_id`),
+  CONSTRAINT `fk_user_1` FOREIGN KEY (`statistic_id`) REFERENCES `statistic` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('2', '2', '2', '2', '2', '2', '1');
+DROP TRIGGER IF EXISTS `cc`;
+DELIMITER ;;
+CREATE TRIGGER `cc` AFTER INSERT ON `comment` FOR EACH ROW begin
+declare c int;
+set c = (select totalreplies from statistic where id = new.stat_id);
+update statistic set totalreplies= c + 1  where id = new.stat_id;
+end
+;;
+DELIMITER ;
+DROP TRIGGER IF EXISTS `comment_trigger_2`;
+DELIMITER ;;
+CREATE TRIGGER `comment_trigger_2` AFTER DELETE ON `comment` FOR EACH ROW begin
+declare c int;
+set c = (select totalreplies from statistic where id = old.stat_id);
+update statistic set totalreplies= c - 1  where id = old.stat_id;
+end
+;;
+DELIMITER ;
+DROP TRIGGER IF EXISTS `aa`;
+DELIMITER ;;
+CREATE TRIGGER `aa` AFTER INSERT ON `topic` FOR EACH ROW begin
+declare c int;
+set c = (select totaltposts from statistic where id = new.statistic_id);
+update statistic set totaltposts = c + 1 where id = new.statistic_id;
+end
+;;
+DELIMITER ;
+DROP TRIGGER IF EXISTS `trigger_3`;
+DELIMITER ;;
+CREATE TRIGGER `trigger_3` BEFORE DELETE ON `topic` FOR EACH ROW BEGIN
+DELETE FROM comment WHERE  topic_id=old.id;
+ END
+;;
+DELIMITER ;
+DROP TRIGGER IF EXISTS `trigger_2`;
+DELIMITER ;;
+CREATE TRIGGER `trigger_2` AFTER DELETE ON `topic` FOR EACH ROW begin
+declare c int;
+set c = (select totaltposts from statistic where id = old.statistic_id);
+update statistic set totaltposts = c - 1 where id = old.statistic_id;
+end
+;;
+DELIMITER ;
+DROP TRIGGER IF EXISTS `bb`;
+DELIMITER ;;
+CREATE TRIGGER `bb` AFTER INSERT ON `user` FOR EACH ROW begin
+declare c int;
+set c = (select totalcommunity from statistic where id = new.statistic_id);
+update statistic set totalcommunity = c + 1 where id = new.statistic_id;
+end
+;;
+DELIMITER ;
+DROP TRIGGER IF EXISTS `user_trigger_3`;
+DELIMITER ;;
+CREATE TRIGGER `user_trigger_3` BEFORE DELETE ON `user` FOR EACH ROW BEGIN
+DELETE FROM topic WHERE  user_id=old.id;
+DELETE FROM comment WHERE  user_id=old.id;
+ END
+;;
+DELIMITER ;
+DROP TRIGGER IF EXISTS `user_trigger_2`;
+DELIMITER ;;
+CREATE TRIGGER `user_trigger_2` AFTER DELETE ON `user` FOR EACH ROW begin
+declare c int;
+set c = (select totalcommunity from statistic where id = old.statistic_id);
+update statistic set totalcommunity = c - 1 where id = old.statistic_id;
+end
+;;
+DELIMITER ;
