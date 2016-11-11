d3263f7b2000d601d28a4a2d82cb5b84ebbd38e4
AfterWin <CJ_Zheng1023@hotmail.com>
2016-11-11 14:20:15 +0800

    修改登录获取不了用户ID的问题

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,5 @@
 module.exports = {
     doLogout: function(req, res,next){
-        console.log(213)
         req.session.destroy(function(err){
             if(err){
                 return next(err);
--- sqlmapping/index-sql.js
+++ sqlmapping/index-sql.js
@@ -2,6 +2,6 @@ module.exports = {
     SELECT_TOPIC_LIST: 'select * from topic',
     SELECT_STATISTIC: 'select * from statistic',
     INSERT_USER: 'insert into user(id,user_name,password,mail) values(?,?,?,?)',
-    VALIDATE_USER: 'SELECT user_name,password,id AS COUNT FROM USER WHERE USER_NAME=? AND PASSWORD=?',
+    VALIDATE_USER: 'SELECT user_name,password,id FROM USER WHERE USER_NAME=? AND PASSWORD=?',
     CHECK_USER: 'select * from user where user_name=?'
 }
