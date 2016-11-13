34b72d4667224159b8d75eed3341187d165f2e63
CJ-Zheng1023 <CJ_Zheng1023@hotmail.com>
2016-11-13 20:47:39 +0800

    文本编辑器功能完成

--- static/scripts/add-topic.js
+++ static/scripts/add-topic.js
@@ -1,7 +1,8 @@
 $(function(){
     $('#content').summernote({
         height: 300,
-        lang: 'zh-CN'
+        lang: 'zh-CN',
+        placeholder: '请输入内容...'
     });
     $('#content').on('summernote.blur', function() {
         $('#content').html($('#content').summernote('code'));
--- static/scripts/detail.js
+++ static/scripts/detail.js
@@ -4,4 +4,14 @@ $(function(){
         var userName = $(this).attr("data-reply-user");
         $("#content").val($("#content").val()+"@"+userName+" ").focus();
     })
+
+
+    $('#content').summernote({
+        height: 300,
+        lang: 'zh-CN',
+        placeholder: '请输入内容...'
+    });
+    $('#content').on('summernote.blur', function() {
+        $('#content').html($('#content').summernote('code'));
+    });
 })
