d1364868222646035087f812cad483cda63e066e
CJ-Zheng1023 <CJ_Zheng1023@hotmail.com>
2016-11-13 20:57:44 +0800

    修复详览文本编辑器BUG

--- static/scripts/detail.js
+++ static/scripts/detail.js
@@ -2,7 +2,7 @@ $(function(){
     $('[data-toggle="tooltip"]').tooltip();
     $("[data-reply-user]").on('click', function(){
         var userName = $(this).attr("data-reply-user");
-        $("#content").val($("#content").val()+"@"+userName+" ").focus();
+        $("#content").summernote('code', $('#content').summernote('code')+"@"+userName+" ");
     })
 
 
