1d576061e2a6344ca4ac9c5d86214dca9f2b9ad6
CJ-Zheng1023 <CJ_Zheng1023@hotmail.com>
2016-11-13 21:11:14 +0800

    首页数据排序

--- sqlmapping/index-sql.js
+++ sqlmapping/index-sql.js
@@ -1,6 +1,6 @@
 module.exports = {
-    SELECT_TOPIC_LIST: 'select t.id,title,page_view,reply_num,user_name,category from topic t,user u where u.id=t.user_id',
-    SELECT_TOPIC_LIST_BY_CATEGORY: 'select t.id,title,page_view,reply_num,user_name from topic t,user u where u.id=t.user_id and category=?',
+    SELECT_TOPIC_LIST: 'select t.id,title,page_view,reply_num,user_name,category from topic t,user u where u.id=t.user_id order by t.create_time desc',
+    SELECT_TOPIC_LIST_BY_CATEGORY: 'select t.id,title,page_view,reply_num,user_name from topic t,user u where u.id=t.user_id and category=? order by t.create_time desc',
     SELECT_STATISTIC: 'select totalcommunity,totaltposts,totalreplies from statistic',
     INSERT_USER: 'insert into user(id,user_name,password,mail) values(?,?,?,?)',
     VALIDATE_USER: 'SELECT user_name,password,id,signature FROM USER WHERE USER_NAME=? AND PASSWORD=?',
