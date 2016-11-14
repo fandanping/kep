f6efd4e10e9dd43e1c324f19fa295567d6370a86
AfterWin <CJ_Zheng1023@hotmail.com>
2016-11-14 08:51:42 +0800

    配置文件修改

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 var config = {
     mysql_database: 'kepdb',
-    mysql_host: 'localhost',
+    mysql_host: '10.113.16.188',
     mysql_port: '3306',
     mysql_user: 'root',
-    mysql_password: 'genius1023',
+    mysql_password: 'root',
     mysql_connectionLimit: 20,
     BASE_WORD:'0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
     category: {
